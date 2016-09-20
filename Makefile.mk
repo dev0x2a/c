7be1efbabeee7608ca5c9283f323a6f3dcf069fe
ryankeleti <elitebananart@gmail.com>
2016-09-20 18:53:20 -0400

    gitupdate.sh >> c

--- Makefile.mk
+++ Makefile.mk
@@ -1,34 +1,32 @@
-include /home/ryanku/Makefile
-
 #		c makefile
 #
 #		ryan keleti
 
-#SRCS:=$(wildcard *.c)
-#OBJS:=${SRCS:.c=.o}
-#NAME:=$(patsubst %.c,%,$(SRCS))
+SRCS:=$(wildcard *.c)
+OBJS:=${SRCS:.c=.o}
+NAME:=$(patsubst %.c,%,$(SRCS))
 
-#INCLUDE_DIR:=
-#LIBRARY_DIR:=
-#LIBS:=
+INCLUDE_DIR:=
+LIBRARY_DIR:=
+LIBS:=
 
-#LDFLAGS +=$(foreach libdir,$(LIBRARY_DIR),-L$(libdir))
-#LDFLAGS +=$(foreach lib,$(LIBS),-l$(lib))
-#CFLAGS:=-lm 
+LDFLAGS +=$(foreach libdir,$(LIBRARY_DIR),-L$(libdir))
+LDFLAGS +=$(foreach lib,$(LIBS),-l$(lib))
+CFLAGS:=-lm 
 
-#.PHONY: all clean distclean remove
+.PHONY: all clean distclean remove
 
-#all: $(OBJS)	
+all: $(OBJS)	
 
-#$(OBJS): %.o: %.c
-#	cc $< -o $@ $(CFLAGS)
+$(OBJS): %.o: %.c
+	cc $< -o $@ $(CFLAGS)
 
-#clean: 
-#	@- $(RM) $(NAME)
-#	@- $(RM) $(OBJS)
+clean: 
+	@- $(RM) $(NAME)
+	@- $(RM) $(OBJS)
 
-#distclean: clean
+distclean: clean
 
-#remove:
-#	rm -i `find . -perm /111 -type f`
+remove:
+	rm -i `find . -perm /111 -type f`
 

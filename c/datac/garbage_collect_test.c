be4c0c1ac81a9d41c69d81244cd5319b3fec7c5b
Ryan Keleti <ryankeleti@users.noreply.github.com>
2016-09-21 09:52:41 -0400

    Update garbage_collect_test.c

--- c/datac/garbage_collect_test.c
+++ c/datac/garbage_collect_test.c
@@ -1,1 +1,22 @@
-#define N
+#define NUMNODES ...;
+#define NUMACC = ...;
+int acc[NUMACC];
+
+struct nodetype {
+  int mark, utype;
+  union {
+    int intgrinfo;
+    char charinfo;
+    int lstinfo;
+  } info;
+  int next;
+} node[NUMNODES];
+
+for (i=0; i<NUMACC; ++i) node[acc[i]].mark = TRUE;
+
+i = 1;
+while (i < NUMNODES) {
+  
+}
+
+

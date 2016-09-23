01f434599b208dc1924ae7f485d46bb6c92334dd
ryankeleti <elitebananart@gmail.com>
2016-09-23 01:08:14 -0400

    gitup.sh >> c

--- src/mod.c
+++ src/mod.c
@@ -1,28 +1,52 @@
 /*
- * add.c
+ * mod.c
  */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
-#define PROGRAM_NAME "add"
+#define PROGRAM_NAME "mod"
 #define COMMAND_NAME "git"
 
 void usage(void);
  
 int main(int argc, char **argv)
 {
-  const char *input_file;
-  const char *args[] = {"add", input_file};
+  const char *input;
+  const char *args[] = {"add", input};
   const char *binary_name = "git";
   char myoutput_array[5000];
   int argcount = 2;
-  int help_check, switchcase;
+  int help_check, switchmod, switchcase;
 
-  printf("%s %s:\t", COMMAND_NAME, PROGRAM_NAME);
-  scanf("%s", input_file);
-  help_check = strcmp(input_file, "help");
+  fprintf(stdout, "\n\
+      1\tadd\n\
+      2\tcommit [-m]\n\
+      3\tpush [<args>]\n\n\
+      _\t\
+      ");
+  scanf("%d", &switchmod);
+
+  if (switchmod == NULL)
+    exit(1);
+
+  switch(switchmod)
+  {
+    case 1: printf("%s add ", COMMAND_NAME);
+            scanf("%s", input);
+            help_check = strcmp(input, "help");
+            break;
+    case 2: printf("%s commit -m ", COMMAND_NAME);
+            scanf("%s", input);
+            help_check = strcmp(input, "help");
+            break;
+    case 3: printf("%s push ", COMMAND_NAME);
+            scanf("%s", input);
+            help_check = strcmp(input, "help");
+            break;
+    default: exit(2);
+  }
 
   if (help_check == 0) switchcase = 1;
   else if (help_check != 0) switchcase = 2;

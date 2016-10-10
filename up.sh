1d7861ae1436d9c1a093c3934fd8a1825867e2e6
ryankeleti <elitebananart@gmail.com>
2016-10-09 22:10:36 -0400

    update Sun Oct  9 22:10:36 EDT 2016

---
+++ up.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+function gitEX () {
+local EX="$?"
+
+  if [ $EX != 0 ]; then
+    echo ":: error"
+  else
+    echo ":: done"
+  fi
+}
+
+cd $HOME/c
+
+files="*"
+
+git add $files 
+git commit -m "update `date`"
+git push origin master
+
+gitEX
+

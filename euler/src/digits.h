556a07f503b5424cfa06db56b3947c24524a6de8
ryankeleti <elitebananart@gmail.com>
2016-10-11 21:33:26 -0400

    update Tue Oct 11 21:33:26 EDT 2016

--- euler/src/digits.h
+++ euler/src/digits.h
@@ -4,16 +4,14 @@
 int
 dgt(int dgt_t, int dgt_n)
 {
-  int dgt_arr[1000];
+  int dgt_arr[200];
   int dgt_i, dgt_j, dgt_tmp, dgt_m, dgt_x;
 
   while (--dgt_t) {
     dgt_arr[0] = 1;
     dgt_m = 1;
-    dgt_i = 2;
-    int dgt_k = 1;
     dgt_tmp = 0;
-    while (dgt_k <= dgt_n) {
+    for (dgt_i=1; dgt_i<=dgt_n; ++dgt_i) {
       for (dgt_j=0; dgt_j<dgt_m; ++dgt_j) {
         dgt_x = dgt_arr[dgt_j]*dgt_i+dgt_tmp;
         dgt_arr[dgt_j] = dgt_x%10;
@@ -24,12 +22,11 @@ dgt(int dgt_t, int dgt_n)
         dgt_tmp /= 10;
         ++dgt_m;
       }
-      ++dgt_k;
     }
     for (dgt_i=dgt_m-1; dgt_i>=0; --dgt_i)
       printf("%d", dgt_arr[dgt_i]);
-    printf("\n");
   }
+  printf("\n");
   return 0;
 }
 

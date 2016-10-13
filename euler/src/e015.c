42a3a642d43616731cd447d976a3e3c43106f222
ryankeleti <elitebananart@gmail.com>
2016-10-12 22:53:01 -0400

    update Wed Oct 12 22:53:01 EDT 2016

--- euler/src/e015.c
+++ euler/src/e015.c
@@ -17,17 +17,17 @@ main(int argc, char *argv[])
   n = atoi(argv[1]);
   ++n;
 
-  unsigned long long g[n][n];
+  unsigned long long a[n][n];
   for (int i=0; i<n; ++i) {
-    g[i][0] = 1;
-    g[0][i] = 1;
+    a[i][0] = 1;
+    a[0][i] = 1;
   }
   for (int i=1; i<n; ++i) {
     for (int j=1; j<n; ++j) {
-      g[i][j] = g[i-1][j] + g[i][j-1];
+      a[i][j] = a[i-1][j] + a[i][j-1];
     }
   }
-  printf("%lld\n", g[n-1][n-1]);
+  printf("%lld\n", a[n-1][n-1]);
 
   end = clock();
   cpu_usage = ((double)(end-start))/CLOCKS_PER_SEC;
---
+++ euler/src/e016.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+long long power(long long n, long long e);
+int modular(int b, unsigned int e, unsigned int m);
+
+int
+main(int argc, char *argv[])
+{
+  unsigned long long b, n;
+  clock_t start, end;
+  double cpu_usage;
+
+  if (argc != 3) {
+    printf("%s <base> <exp>\n", argv[0]);
+    return(-1);
+  }
+  start = clock();
+  b = atoi(argv[1]);
+  n = atoi(argv[2]);
+
+  printf("%lld\n", modular(2,1000,10));
+
+  end = clock();
+  cpu_usage = ((double)(end-start))/CLOCKS_PER_SEC;
+  printf("cpu time: %1f\n", cpu_usage);
+  return 0;
+}
+
+long long 
+power(long long n, long long e)
+{
+  long long r;
+  r = 1;
+  while (e > 0) {
+    if ((e&1) != 0)
+      r *= n;
+    n *= n;
+    e >>= 1;
+  }
+  return r;
+}
+  
+int
+modular(int b, unsigned int e, unsigned int m)
+{
+  int i;
+  int r = 1;
+  int p = b%m;
+
+  for (i=0; i<sizeof(int)*8; ++i) {
+    int lbit = 0x00000001&(e>>i);
+    if (lbit)
+      r = (r*p)%m;
+  }
+  return r;
+}
+  

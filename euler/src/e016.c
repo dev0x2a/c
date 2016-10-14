fcbb5d5890325712295dc67d23121a1c265d3b9b
ryankeleti <elitebananart@gmail.com>
2016-10-14 19:27:41 -0400

    update Fri Oct 14 19:27:41 EDT 2016

--- euler/src/e016.c
+++ euler/src/e016.c
@@ -1,25 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <gmp.h>
 
-unsigned long long power(unsigned long long n, unsigned long long e);
+
+unsigned int opt[] =
+{
+  1,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,
+};
 
 int
 main(int argc, char *argv[])
 {
-  unsigned long long b, n;
-  clock_t start, end;
-  double cpu_usage;
-
   if (argc != 3) {
     printf("%s <base> <exp>\n", argv[0]);
     return(-1);
   }
+  int b, n;
+  clock_t start, end;
+  double cpu_usage;
+  mpz_t ret;
+
   start = clock();
   b = atoi(argv[1]);
   n = atoi(argv[2]);
 
-  printf("%d^%d = %lld\n", b, n, power(b,n));
+  mpz_init(ret);
+  mpz_ui_pow_ui(ret, b, n);
+  printf("%d^%d = ", b, n);
+  mpz_out_str(NULL, 10, ret);
+  printf("\n");
+  mpz_clear(ret);
 
   end = clock();
   cpu_usage = ((double)(end-start))/CLOCKS_PER_SEC;
@@ -27,17 +38,3 @@ main(int argc, char *argv[])
   return 0;
 }
 
-unsigned long long 
-power(unsigned long long n, unsigned long long e)
-{
-  unsigned long long r;
-  r = 1;
-  while (e > 0) {
-    if ((e&1) != 0)
-      r *= n;
-    n *= n;
-    e >>= 1;
-  }
-  return r;
-}
-  

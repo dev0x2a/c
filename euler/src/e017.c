ab3feac41a480108d8868e71e8b0196feb021f7d
ryankeleti <elitebananart@gmail.com>
2016-10-17 00:23:54 -0400

    update Mon Oct 17 00:23:54 EDT 2016

--- euler/src/e017.c
+++ euler/src/e017.c
@@ -2,40 +2,60 @@
 #include <stdlib.h>
 #include <time.h>
 
-//#define LIM 1000
-int wheel[10] = {
-  3,3,5,4,4,3,5,5,4,3
+int dec_wheel[20] = {
+  3,3,5,4,4,3,5,5,4,3,0,
 };
+
+int spec_wheel[20] = {
+  6,6,7,7,7,7,9,9,8,0,
+};
+
+int tens_wheel[20] = {
+  6,6,6,5,5,7,6,6,1,0,
+};
+
 
 int
 main(int argc, char *argv[])
 {
-  if (argc != 3) {
-    printf("%s <llim> <ulim>\n", argv[0]);
+  if (argc != 2) {
+    printf("%s <lim>\n", argv[0]);
     return(-1);
   }
-  int l, u, s;
+  int lim, s;
   clock_t start, end;
   double cpu_usage;
 
   start = clock();
-  l = atoi(argv[1]);
-  u = atoi(argv[2]);
+  lim = atoi(argv[1]);
 
-  if (l > u) {
-    printf("%s llim > ulim\n", argv[0]);
+  if (lim < 1) {
+    printf("%s lim < 1\n", argv[0]);
     return(-1);
   }
 
   s = 0;
-  for (int i=l; i<u; ++i) {
-    s += wheel[j];
-    printf("%d ", s);
+  for (int i=0; i<lim; ++i) {
+    int m, n, k, j;
+    for (m=0; dec_wheel[m]!=0, m<lim; ++m) {
+      s += dec_wheel[m];
+    }
+    if (lim<11) break;
+    for (n=0; spec_wheel[n]!=0, n<lim-10; ++n) {
+      s += spec_wheel[n];
+    }
+    if (lim<21) break;
+    for (k=0, j=0;; ++j) {
 
-  
+      s += tens_wheel[k] + dec_wheel[j];
+      if (j>9) {
+        j = 0;
+        ++k;
+      }
+    }
   }
-
-
+  printf("%d\n", s);
+  
   end = clock();
   cpu_usage = ((double)(end-start))/CLOCKS_PER_SEC;
   printf("\ncpu time: %1f\n", cpu_usage);

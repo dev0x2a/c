6113335c7320cd44e23996820330db60c684c9dc
ryankeleti <elitebananart@gmail.com>
2016-10-21 16:08:35 -0400

    update Fri Oct 21 16:08:35 EDT 2016

--- euler/math/m_/collatz.c
+++ euler/math/m_/collatz.c
@@ -19,17 +19,18 @@ main(int argc, char *argv[])
   }
   c = 0;
   while (N!=1) {
-    printf("%llu ", N);
+    //printf("%llu ", N);
     
     if (N&1) {
-      N = (3*N+1)>>1;
+      N=(((N<<1)+N)+1)>>1;
+      //N=(3*N+1)>>1;
       c+=2;
     } else {
-      N >>= 1;
+      N>>=1;
       ++c;
     }
   }
-  printf("1");
+  //printf("1");
   printf("\n\033[0;31mcount = %d\033[0m\n\n", c);
   return(0);
 }

f2da6b15bba43b6a163b5621d347fdeafdec4472
ryankeleti <elitebananart@gmail.com>
2016-10-25 19:25:50 -0400

    update Tue Oct 25 19:25:50 EDT 2016

--- obin.c
+++ obin.c
@@ -2,6 +2,6 @@
 #define i write
 #define n int
 #define a for
-#define r "1"
-#define y "0"
-b{n k,l=atoi(z[1]);a(k=31;k>=0;--k){((l>>k)&1)?i(1,r,1):i(1,y,1);}puts("");}
+#define r puts("")
+#define y atoi
+b{n k,l=y(z[1]);a(k=31;k>=0;--k){((l>>k)&1)?i(1,"1",1):i(1,"0",1);}r;}

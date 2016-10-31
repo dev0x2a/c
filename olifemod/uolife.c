3df3ac3f142d46cdef4aedef670b44d2d688c170
ryankeleti <elitebananart@gmail.com>
2016-10-31 07:04:56 -0400

    update Mon Oct 31 07:04:56 EDT 2016

--- olifemod/uolife.c
+++ olifemod/uolife.c
@@ -9,6 +9,7 @@
  *  orig @theduckscientist
  */
 const int rate=20000;
+const char *str="\x01?@A";
 
 int main(int argc,char *argv[])
 {
@@ -32,27 +33,33 @@ int main(int argc,char *argv[])
   int n=~n+n;
   r=(int)&r;
 
-  while((++n)^2048)
+  while((++n)^2048){
     n[a]=(r=(r*0x41C64E6D+12345)&0x7fffffff)&1024?1<<5:0;
-  
+  }
+
   //while(usleep('d'<<'\n'),n+=~n){
   while(usleep(rate),n+=~n){
 
     while((++n)^8192){
 
-      if(a[((n>>2)+2048+"\x01?@A"[n%4])%04000]&1<<5)
+      if(a[((n>>2)+2048+str[n%4])%04000]&1<<5){
         *(a+(n>>2))=-~a[n>>2];
+      }
 
-      if(a[((n>>2)+04000-"\x01?@A"[n%4])%2048]&1<<5)
+      if(a[((n>>2)+04000-str[n%4])%2048]&1<<5){
         *(a+(n>>2))=-~a[n>>2];
+      }
 
-      if(n%4==3)
+      if(n%4==3){
         a[n>>2]|=a[n>>2]^'\"'&&a[n>>2]^'#'?((a[n>>2]^3)?0:1<<4):1<<4;
+      }
     }
-    while((++n)^10240)
+    while((++n)^10240){
 
-      if(putchar((a[n%2048]=a[n%2048]<<1&1<<5)?out:bg),!(63^n%0100))
+      if(putchar((a[n%2048]=a[n%2048]<<1&1<<5)?out:bg),!(63^n%0100)){
         putchar(10);
+      }
+    }
   }
 }
 

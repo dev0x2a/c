a867402c7c211225d7dbab46192b3ad4b60bd22d
ryankeleti <elitebananart@gmail.com>
2016-11-05 23:10:32 -0400

    update Sat Nov  5 23:10:32 EDT 2016

--- src/euler/src/e017.c
+++ src/euler/src/e017.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #define S 40
 
-int dec_whl[S] = {
+int dec_whl[S]={
   3,3,5,4,4,3,5,5,4,3
 };
-int spec_whl[S] = {
+int spec_whl[S]={
   6,6,8,8,7,7,9,9,8,6
 };
-int tens_whl[S] = {
+int tens_whl[S]={
   6,6,6,5,5,7,6,6
 };
-int hund_whl[S] = {0,7};
+int hund_whl[S]={0,7};
 
 int
 main(void)
@@ -18,30 +18,32 @@ main(void)
   int i,h;
   unsigned long long s;
 
-  while (i<1001) {
+  while(i<1001){
     i=0;
     h=0;
     s=0;
-A:  for (int n=0; n<10; ++n,++i) {
+A:  for(int n=0; n<10; ++n,++i){
       s+=dec_whl[n]+hund_whl[h];
     }
-    for (int n=0; n<10; ++n,++i) {
+    for(int n=0; n<10; ++n,++i){
       s+=spec_whl[n];
     }
-    for (int n=0,k=0; n<8; ++k,++i) {
-      if (k>9) { k=0; ++n; }
+    for(int n=0,k=0; n<8; ++k,++i){
+      if(k>9){ 
+        k=0;++n;
+      }
       s+=tens_whl[n]+dec_whl[k];
     }
-    if (i>=1000) {
+    if(i>=1000){
       s+=8+dec_whl[0];
       break;
     }
-    if (i%100) {
+    if(i%100){
       s+=3*99;
       h=1;
       goto A;
     }  
-    for (int n=0; n<9; ++n) {
+    for(int n=0; n<9; ++n){
       s+=dec_whl[n]+7;
     }
   }

3134e611248987e7d19f3bea3a0e0f15bbe2371b
ryankeleti <elitebananart@gmail.com>
2016-11-08 13:34:25 -0500

    update Tue Nov  8 13:34:25 EST 2016

--- src/prim/primefile.c
+++ src/prim/primefile.c
@@ -5,14 +5,13 @@ int main(int argc, char *argv[])
   int i,k;
   unsigned long long n;
   char c;
-  FILE *fp,*fc;
+  FILE *fp;
 
   if (argc < 2) {
     fprintf(stderr, "%s [FILE]\n", argv[0]);
     return(-1);
   }
   fp = fopen(argv[1], "r");
-  fc = fopen(argv[1], "r");
 
   if (!fp) {
     fprintf(stderr, "%s: %s cannot be opened\n", argv[0],fp);
@@ -20,20 +19,23 @@ int main(int argc, char *argv[])
   }
 
   k = 1;
-  while (fscanf(fc, "%c", &c) != EOF) {
+  while (fscanf(fp, "%c", &c) != EOF) {
     if (c == ',')
-      k+=2;
+      ++k;
   }
-  fclose(fc);
+  fclose(fp);
   printf("\ninit p[%d]\n\n", k);
 
   unsigned long long p[k];
+  fp = fopen(argv[1], "r");
   i = 0;
+  
   while (fscanf(fp, "%llu,", &n) > 0) {
     p[++i] = n;
     printf("%llu\n", p[i++]);
   }
 
+  putchar('\n');
   fclose(fp);
   return(0);
 }

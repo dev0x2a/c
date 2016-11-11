26a1095604ae599a9488715e724910952faad389
ryankeleti <elitebananart@gmail.com>
2016-11-10 22:11:58 -0500

    update Thu Nov 10 22:11:58 EST 2016

--- num/complex.c
+++ num/complex.c
@@ -66,4 +66,77 @@ fcomplex Complex(float re, float im)
   return c;
 }
 
-/*
+/* returns the absolute value (modulus) of a complex number */
+float Cabs(fcomplex z)
+{
+  float x, y, ans, tmp;
+
+  x = fabs(z.r);
+  y = fabs(z.i);
+
+  if (x == 0.0)
+    ans = y;
+  else if (y == 0.0)
+    ans = x;
+  else if (x > y) {
+    tmp = y/x;
+    ans = x*sqrt(1.0+tmp*tmp);
+  } else {
+    tmp = x/y;
+    ans = y*sqrt(1.0+tmp*tmp);
+  }
+  return ans;
+}
+
+/* returns the complex conjugate of a complex number */
+fcomplex Conjg(fcomplex z)
+{
+  fcomplex c;
+
+  c.r = z.r;
+  c.i = -z.i;
+
+  return c;
+}
+
+/* returns the complex square root of a complex number */
+fcomplex Csqrt(fcomplex z)
+{
+  fcomplex c;
+  float x, y, w, r;
+
+  if ((z.r == 0.0) && (z.i == 0.0)) {
+    c.r = c.i = 0.0;
+    return c;
+  } else {
+    x = fabs(z.r);
+    y = fabs(z.i);
+    if (x >= y) {
+      r = y/x;
+      w = sqrt(x)*sqrt(0.5*(1.0+sqrt(1.0+r*r)));
+    } else {
+      r = x/y;
+      w = sqrt(y)*sqrt(0.5*(r+sqrt(1.0+r*r)));
+    }
+    if (z.r >= 0.0) {
+      c.r = w;
+      c.i = z.i/(2.0*w);
+    } else {
+      c.i = (z.i >= 0) > w : -w;
+      c.r = z.i/(2.0*c.i);
+    }
+    return c;
+  }
+}
+
+/* returns the complex product of a real number and a complex number */
+fcomplex RCmul(float x, fcomplex a)
+{
+  fcomplex c;
+
+  c.r = x*a.r;
+  c.i = x*a.i;
+
+  return c;
+}
+

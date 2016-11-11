18df0784b21dd7ef4a580cfaee8a12e0999b0e29
ryankeleti <elitebananart@gmail.com>
2016-11-11 01:56:13 -0500

    update Fri Nov 11 01:56:13 EST 2016

--- alg/insertsort.c
+++ alg/insertsort.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "alg.h"
 
 int main(int argc, char *argv[])
@@ -6,18 +7,17 @@ int main(int argc, char *argv[])
   int i, j, key;
 
   unsigned int k = m_atoi(argv[1]);
-  unsigned long int a[k];
+  int *a;
+  a = (int *)malloc(k*sizeof(int));
   printf("a[%d] = {\n\t", k);
 
-  //m_srand(k);
-
   for (i=0; i<k; ++i) {
-    a[i] = m_rand();
-    printf("%ld ", a[i]);
+    a[i] = m_rand(); 
+    printf("%d ", a[i]);
   }
   puts("\n}\nsort:\n");
 
-  for (j=2; j<k; ++j) {
+  for (j=2,i=0; j<k; ++j) {
     key = a[j];
     /* insert a[j] into the sorted seq [1..j-1] */
     i = j-1;
@@ -27,6 +27,12 @@ int main(int argc, char *argv[])
     }
     a[i+1] = key;
   }
+  printf("%d ", a[0]);
+#if 0
+  for (i=0; i<k; ++i) {
+    printf("%d ", a[i]);
+  }
+#endif
   putchar('\n');
 
 }

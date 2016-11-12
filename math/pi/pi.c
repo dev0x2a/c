769b9fbeaa4ff89d1d42fd49392da8d562ea793b
ryankeleti <elitebananart@gmail.com>
2016-11-12 10:49:13 -0500

    update Sat Nov 12 10:49:13 EST 2016

--- math/pi/pi.c
+++ math/pi/pi.c
@@ -1,3 +1,19 @@
+/*
+ * Computation of the n'th decimal digit of \pi with very little memory.
+ * Written by Fabrice Bellard on January 8, 1997.
+ * 
+ * We use a slightly modified version of the method described by Simon
+ * Plouffe in "On the Computation of the n'th decimal digit of various
+ * transcendental numbers" (November 1996). We have modified the algorithm
+ * to get a running time of O(n^2) instead of O(n^3log(n)^3).
+ * 
+ * This program uses mostly integer arithmetic. It may be slow on some
+ * hardwares where integer multiplications and divisons must be done
+ * by software. We have supposed that 'int' has a size of 32 bits. If
+ * your compiler supports 'long long' integers of 64 bits, you may use
+ * the integer version of 'mul_mod' (see HAS_LONG_LONG).  
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>

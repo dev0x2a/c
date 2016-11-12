be9922ac5fcd3d1064d296915a5eaf108104c57c
ryankeleti <elitebananart@gmail.com>
2016-11-12 07:35:11 -0500

    update Sat Nov 12 07:35:11 EST 2016

--- prim/tiny.c
+++ prim/tiny.c
@@ -1,19 +1,22 @@
-#if 0
+#if 1
 #include <stdio.h>
+/* 
+ * added for printf() calls
+ */
 #endif
-
 #define TPI_1   0
 #define TPI_2   0
 #define TPI_3   0
-
 #define TE_1    0
-
 #define TLOG2_1 0
 #define TLOG2_2 0
-
 #define T_SQ2_1 0
-
-#define T_GR_1  1
+#define T_GR_1  0
+#define T_GEN_1 0
+#define T_EUL_1 0
+#define T_PRI_1 0
+#define T_PRI_2 0
+#define T_FFT_1 1
 
 #if TPI_1
 int a=10000,b,c=8400,d,e,f[8401],g;main(){
@@ -67,4 +70,36 @@ main(){int a=1000,b=0,c=3434,d,f[3435],n=800,k;
 for(;b<c;f[b++]=1);f[1]+=5;
 for(;n--;d+=*f*a,printf("%.3d",d/a),*f=d%a)
 for(d=0,k=c;--k;d/=b,d*=2*k-1)f[k]=(d+=f[k]*a)%(b=10*k);}
+#endif
+
+#if T_GEN_1
+main(){for(d=0,k=c;--k;d/=b,d*=2*k-1)f[k]=(d+=f[k]*a)%(b=100*k);}
+#endif
+
+#if T_EUL_1
+int*d,*e,w[2850],p=475,b=10000,r=3,i,j,x;s(int k){d=w+k/14%4*p;
+e=w+k/56*p;for(x=k&1,j=k/2%7*p;j--;k&1?*d=(x+=k-49?*e++-*d+b-1:
+(r?r-1?4096:3888:4374)**d)%b,d++,x/=b:(x=x%i*b+e[j],d[j]=x/i));
+}main(){for(;r<3&i++<33*p+!r?s(216),s(49),s(242),s(59),1:r?i=0,
+w[5*p]=--r?r-1?23:84:60:printf("%.4d",w[--p])&&p>53;);}
+#endif
+
+#if T_PRI_1
+main(){for(int p=1,i;++p<1e5;i>p?printf("%d ",p):0)for(i=2;p%i++;);}
+#endif
+
+#if T_PRI_2
+main(){int a[100000],i=1e5,n=i,p=1;while(--i)a[i]=1;
+while(++p<n)for(a[p]?printf("%d ",i=p):0;p<4e4&&i*p<n;a[i++*p]=0);}
+#endif
+
+#if T_FFT_1
+int m=754974721,N,t[1<<22],a,*p,i,e=4625195,j,s,b,c,U;f(d){for(s
+=1<<23;s;s/=2,d=d*1LL*d%m)if(s<N)for(p=t;p<t+N;p+=s)for(i=s,c=1;
+i;i--)b=*p+p[s],p[s]=(m+*p-p[s])*1LL*c%m,*p++=b%m,c=c*1LL*d%m;
+for(j=0;i<N-1;){for(s=N/2;!((j^=s)&s);s/=2);if(++i<j)a=t[i],t[i]
+=t[j],t[j]=a;}}main(){*t=2;U=N=1;while(e/=2){N*=2;U=U*1LL*(m+1)/
+2%m;f(362);for(p=t;p<t+N;)*p++=*p*1LL**p%m*U%m;f(415027540);for(
+a=0,p=t;p<t+N;)a+=*p<<(e&1),*p++=a%10,a/=10;}while(!*--p);t[0]--
+;while(p>=t)printf("%d",*p--);}
 #endif

d0f331ebff1744c4829e6520d9abcc2d9475ad55
ryankeleti <elitebananart@gmail.com>
2016-11-16 19:09:45 -0500

    update Wed Nov 16 19:09:45 EST 2016

---
+++ math/alg/pearls/rotate.c
@@ -0,0 +1,13 @@
+/*
+ * rotate.c: time alg for rotating a vector
+ * (c)1999 Lucent Tech
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAXN 10000000
+
+int x[MAXN];
+int rotdist, n;
--- math/bitflags.c
+++ math/bitflags.c
@@ -2,14 +2,18 @@
 #define BITFLAG1 (1)
 #define BITFLAG2 (2)
 #define BITFLAG3 (4)
+
 int main(void)
-{ unsigned bitbucket=0U;
-  bitbucket|=BITFLAG1;
-  bitbucket&=~BITFLAG2;
-  bitbucket^=BITFLAG3;
-  if(bitbucket&BITFLAG3){
+{
+  unsigned bitbucket=0U;
+  
+  bitbucket |= BITFLAG1;
+  bitbucket &= ~BITFLAG2;
+  bitbucket ^= BITFLAG3;
+  
+  if (bitbucket & BITFLAG3) {
     return(1);
-  }else{
+  } else {
     return(2);
   }
   return(0);

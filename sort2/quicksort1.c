a5bcbf86fed1426ff25c6cbdcb0d61adb08e44dd
ryankeleti <elitebananart@gmail.com>
2016-11-22 07:23:17 -0500

    update Tue Nov 22 07:23:17 EST 2016

---
+++ sort2/quicksort1.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAXSTACK 100
+
+void partition(int x[], int lb, int ub, int *pj)
+{
+  int a, down, up, tmp;
+  a = x[lb];
+  up = ub;
+  down = lb;
+  while (down < up) {
+    while (x[down]<=a && down<ub)
+      down++;
+    while(x[up] > a)
+      up--;
+    if (down < up) {
+      tmp = x[down];
+      x[down] = x[up];
+      x[up] = tmp;
+    }
+  }
+  x[lb] = x[up];
+  x[up] = a;
+  *pj = up;
+}
+
+void quicksort(int x[], int n)
+{
+  int i, j;
+  struct bndtype {
+    int lb;
+    int ub;
+  } newbnds;
+  struct {
+    int top;
+    struct bndtype bounds[MAXSTACK];
+  } stack;
+  stack.top = -1;
+  newbnds.lb = 0;
+  newbnds.ub = n-1;
+  push(&stack, &newbnds);
+  while (!empty(&stack)) {
+    popsub(&stack, &newbnds);
+    while (newbnds.ub > newbnds.lb) {
+      partition(x, newbnds.lb, newbnds.ub, &j);
+      if (j-newbnds.lb > newbnds.ub-j) {
+        i = newbnds.ub;
+        newbnds.ub = j-1;
+        push(&stack, &newbnds);
+        newbnds.lb = j+1;
+        newbnds.ub = i;
+      } else {
+        i = newbnds.lb;
+        newbnds.lb = j+1;
+        push(&stack, &newbnds);
+        newbnds.lb = i;
+        newbnds.ub = j-1;
+      }
+    }
+  }
+}
+
+

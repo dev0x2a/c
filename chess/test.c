560820459626d4d84248fcacb20919a6eee2534c
ryankeleti <elitebananart@gmail.com>
2016-11-24 08:09:36 -0500

    update Thu Nov 24 08:09:36 EST 2016

---
+++ chess/test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void)
+{
+  int n;
+  n=0;
+  printf("%d  %d  %d\n", n, ~n, !n);
+  return 0;
+}
+
--- chess/ttt.c
+++ chess/ttt.c
@@ -5,39 +5,41 @@ enum { X=0,O };
 enum { OFF=0, ON };
 
 typedef struct user {
-  char stat:2;
-  char turn:2;
-  char who:2;
+  char *stat;
+  char *turn;
+  char *who;
 } *usr_t;
 
 void pboard(char board[3][3])
 {
   char i, j;
+  putchar('\n');
   for (i=0; i<3; ++i) {
     for (j=0; j<3; ++j) {
       printf("%c ", board[i][j]);
     }
     printf("%d\n", i+1);
   }
-  puts("a b c");
+  puts("a b c\n");
 }
 
 int main(void)
 {
   char board[3][3] = {'.','.','.','.','.','.','.','.','.'};
-  char n = 0;
+  char n;
 
   usr_t p1, p2;
 
   puts("x(0) or o(1)?");
-  //scanf("%c", &n);
-  p1->who = n;
-  //p2->who = n;
-
-puts("x goes first");
+  scanf("%d", &n);
+  p1->who = &n;
+  //p2->who = !n;
+  
+  printf("%c goes first\n", p1->who ? 'o' : 'x');
+  printf("%c", p2->who);
   //p1->turn = ON;
   //p2->turn = OFF;
-  pboard(board);
+  //pboard(board);
 
 
 

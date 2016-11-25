fd180498813851a42cd58253c126667396876848
ryankeleti <elitebananart@gmail.com>
2016-11-25 13:47:14 -0500

    update Fri Nov 25 13:47:14 EST 2016

--- chess/chess.c
+++ chess/chess.c
@@ -15,16 +15,42 @@ int main(void)
   black->turn = 0;
   black->status = 1;
 
-
-#if 0
-  psc_t rw1, rw2, kw1, kw2, bw1, bw2, qw,  kw;
-  psc_t pw1, pw2, pw3, pw4, pw5, pw6, pw7, pw8;
-  psc_t rb1, rb2, kb1, kb2, bb1, bb2, qb,  kb;
-  psc_t pb1, pb2, pb3, pb4, pb5, pb6, pb7, pb8;
-#endif
   psc_t rw1 = initpiece(ROOK, ALIVE, WH, H, 1);
   psc_t rw2 = initpiece(ROOK, ALIVE, WH, A, 1);
+  psc_t nw1 = initpiece(KNIGHT, ALIVE, WH, G, 1);
+  psc_t nw2 = initpiece(KNIGHT, ALIVE, WH, B, 1);
+  psc_t bw1 = initpiece(BISHOP, ALIVE, WH, F, 1);
+  psc_t bw2 = initpiece(BISHOP, ALIVE, WH, C, 1);
+  psc_t qww = initpiece(QUEEN, ALIVE, WH, D, 1);
+  psc_t kww = initpiece(KING, ALIVE, WH, E, 1);
+  psc_t pw1 = initpiece(PAWN, ALIVE, WH, H, 2);
+  psc_t pw2 = initpiece(PAWN, ALIVE, WH, G, 2);
+  psc_t pw3 = initpiece(PAWN, ALIVE, WH, F, 2);
+  psc_t pw4 = initpiece(PAWN, ALIVE, WH, E, 2);
+  psc_t pw5 = initpiece(PAWN, ALIVE, WH, D, 2);
+  psc_t pw6 = initpiece(PAWN, ALIVE, WH, C, 2);
+  psc_t pw7 = initpiece(PAWN, ALIVE, WH, B, 2);
+  psc_t pw8 = initpiece(PAWN, ALIVE, WH, A, 2);
   
+
+  psc_t rb1 = initpiece(ROOK, ALIVE, BL, A, 1);
+  psc_t rb2 = initpiece(ROOK, ALIVE, BL, H, 1);
+  psc_t nb1 = initpiece(KNIGHT, ALIVE, BL, B, 1);
+  psc_t nb2 = initpiece(KNIGHT, ALIVE, BL, G, 1);
+  psc_t bb1 = initpiece(BISHOP, ALIVE, BL, C, 1);
+  psc_t bb2 = initpiece(BISHOP, ALIVE, BL, F, 1);
+  psc_t qbb = initpiece(QUEEN, ALIVE, BL, E, 1);
+  psc_t kbb = initpiece(KING, ALIVE, BL, D, 1);
+  psc_t pb1 = initpiece(PAWN, ALIVE, BL, A, 2);
+  psc_t pb2 = initpiece(PAWN, ALIVE, BL, B, 2);
+  psc_t pb3 = initpiece(PAWN, ALIVE, BL, C, 2);
+  psc_t pb4 = initpiece(PAWN, ALIVE, BL, D, 2);
+  psc_t pb5 = initpiece(PAWN, ALIVE, BL, E, 2);
+  psc_t pb6 = initpiece(PAWN, ALIVE, BL, F, 2);
+  psc_t pb7 = initpiece(PAWN, ALIVE, BL, G, 2);
+  psc_t pb8 = initpiece(PAWN, ALIVE, BL, H, 2);
+
+
 
   printuser(white);
   printuser(black);

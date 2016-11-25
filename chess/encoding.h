04202e488809c223bd4520db4a19c8ee6bcd5ec8
ryankeleti <elitebananart@gmail.com>
2016-11-25 12:04:42 -0500

    update Fri Nov 25 12:04:42 EST 2016

--- chess/encoding.h
+++ chess/encoding.h
@@ -1,13 +1,38 @@
-0x0 //empty
-0x1c0 //pawn
-0x1c100 //rook
-0x1c101 //knight
-0x1c110 //bishop
-0x1c1110 //queen
-0x1d1111 //king
+#define EEMPTY 0x0 //empty
+#define EPAWN 0x1c0 //pawn
+#define EROOK 0x1c100 //rook
+#define EKNIGHT 0x1c101 //knight
+#define EBISHOP 0x1c110 //bishop
+#define EQUEEN 0x1c1110 //queen
+#define EKING 0x1d1111 //king
 
-32 //empty board 32*1 bit
-48 //pawns 16*3 bits
-60 //rooks/knights/bishops 12*5 bits
-24 //queens/kings 4*6 bits
-164 //total initial state
+#define EBEMPTY 32 //empty board 32*1 bit
+#define EBEPAWN 48 //pawns 16*3 bits
+#define EBELSE 60 //rooks/knights/bishops 12*5 bits
+#define EBSPEC 24 //queens/kings 4*6 bits
+#define EBINIT 164 //total initial state
+
+typedef unsigned int uint;
+uint move;
+
+void pmove(uint from, uint to, uint flag) {
+  move = ((flag & 0xf)<<12) | ((from & 0x3f)<<6) | (to & 0x3f);
+}
+
+uint getto() {return move & 0x3f;}
+uint getfrom() {return (move >> 6) & 0x3f;}
+uint getflag() {return (move >> 12) & 0x0f;}
+
+void setto(uint to) {
+  move &= ~0x3f;
+  move |= to & 0x3f;
+}
+
+void setfrom(uint from) {
+  move &= ~0xfc0;
+  move |= (from & 0x3f) << 6;
+}
+
+int capture() {
+  return (move & CAPTURE) != 0;
+}

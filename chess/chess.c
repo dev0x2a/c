4f869b54a5bf2b2f26cefed5177f06440358d788
ryankeleti <elitebananart@gmail.com>
2016-11-25 11:26:24 -0500

    update Fri Nov 25 11:26:24 EST 2016

--- chess/chess.c
+++ chess/chess.c
@@ -15,26 +15,23 @@ int main(void)
   black->turn = 0;
   black->status = 1;
 
-  printuser(white);
-  printuser(black);
 
 #if 0
-  psc_t kw1, kw2;
-  psc_t bw1, bw2;
-  psc_t quw, kiw;
+  psc_t rw1, rw2, kw1, kw2, bw1, bw2, qw,  kw;
   psc_t pw1, pw2, pw3, pw4, pw5, pw6, pw7, pw8;
-  psc_t rb1, rb2;
-  psc_t kb1, kb2;
-  psc_t bb1, bb2;
-  psc_t qub, kib;
+  psc_t rb1, rb2, kb1, kb2, bb1, bb2, qb,  kb;
   psc_t pb1, pb2, pb3, pb4, pb5, pb6, pb7, pb8;
 #endif
   psc_t rw1 = initpiece(ROOK, ALIVE, WH, H, 1);
   psc_t rw2 = initpiece(ROOK, ALIVE, WH, A, 1);
   
 
+  printuser(white);
+  printuser(black);
+  
   printinfo(rw1);
   printinfo(rw2);
+
   FREE(white, black);
   FREE(rw1, rw2);
 

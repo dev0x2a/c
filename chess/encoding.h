1b9a51c5adaa85b0449f7311bf3cd6b011c50f68
ryankeleti <elitebananart@gmail.com>
2016-11-25 11:51:30 -0500

    update Fri Nov 25 11:51:30 EST 2016

---
+++ chess/encoding.h
@@ -0,0 +1,13 @@
+0x0 //empty
+0x1c0 //pawn
+0x1c100 //rook
+0x1c101 //knight
+0x1c110 //bishop
+0x1c1110 //queen
+0x1d1111 //king
+
+32 //empty board 32*1 bit
+48 //pawns 16*3 bits
+60 //rooks/knights/bishops 12*5 bits
+24 //queens/kings 4*6 bits
+164 //total initial state

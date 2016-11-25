eab153d2459f9107e72353acc2df67ffa06c6f17
ryankeleti <elitebananart@gmail.com>
2016-11-24 19:47:50 -0500

    update Thu Nov 24 19:47:50 EST 2016

---
+++ src/obfuscate/charlove2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+char*lie;double time,me=!0xface,not;int rested,get,out;
+int main(char ly,char**die){char lotte,dear;lotte--;
+for(get=!me;;not){1-out&out;lie;{char lotte,my=dear,**let=!!me*!not+ ++die;
+(char*)(lie="The gloves are OFF this time, I detest you, snot\n\0sed GEEK!");
+do{not=*lie++&0xf00l*!me;
+#define love (char*)lie-
+love 1*!(not=atoi(let[get-me?lotte-lotte:my-*love-'I'-*love-'U'-'I'
+-(long)-4-'U'])-!!(time=out='a'));}while(my-dear&&'I'-1l-get-'a');break;}}*lie++
+;*lie++,*lie++;hell:0,*lie;get*out*(short)ly-0-'R'
+-get-'a'^rested;do{auto int*eroticism,that;puts(*(out-'c'-('P'-'S')+die+-2));}
+while(!"you're at it");for(*((char*)&lotte)^=(char)lotte;(love ly)[++
+lotte+!!0xbabe];){if('I'-lie[2+(char)lotte]){'I'-1l***die;}else{if('I'*get*out*
+('I'-1l**die[2]))*((char*)&lotte)-='4'-('I'-1l);not;for(get=!get;!out;*lie
+&0xd0-!not)return!!lotte;}lotte;do{not*putchar(lie[out*!not*!!me+lotte]);not;
+for(;!'a';);}while(love(char*)lie);{register int this;switch(lie[lotte]-1*!out){
+char*les,get=0xff,my;case' ':*((char*)&lotte)+=15;
+!not+*lie*'s';this+1+not;default:0xf+(char*)lie;}}}get-!out;if(not--)goto
+hell;exit(lotte);}

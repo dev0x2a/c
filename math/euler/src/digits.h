9485b9cf95adb78e841d31ac0db689bde454a2a1
ryankeleti <elitebananart@gmail.com>
2016-11-26 00:47:06 -0500

    update Sat Nov 26 00:47:06 EST 2016

--- math/euler/src/digits.h
+++ math/euler/src/digits.h
@@ -1,53 +1,54 @@
 #ifndef DIGITS_H
 #define DIGITS_H
 
-int dgt(int dgt_t, int dgt_n)
+#include <stdlib.h>
+
+int dgt(int t, int n)
 {
-  int dgt_arr[200];
-  int dgt_i, dgt_j, dgt_tmp, dgt_m, dgt_x;
-  while (--dgt_t) {
-    dgt_arr[0]=1;
-    dgt_m=1;
-    dgt_tmp=0;
-    for (dgt_i=1; dgt_i<=dgt_n; ++dgt_i) {
-      for (dgt_j=0; dgt_j<dgt_m; ++dgt_j) {
-        dgt_x=dgt_arr[dgt_j]*dgt_i+dgt_tmp;
-        dgt_arr[dgt_j]=dgt_x%10;
-        dgt_tmp=dgt_x/10;
+  int arr[200];
+  int i, j, tmp, m, x;
+  while (--t) {
+    arr[0]=1;
+    m=1;
+    tmp=0;
+    for (i=1; i<=n; ++i) {
+      for (j=0; j<m; ++j) {
+        x=arr[j]*i+tmp;
+        arr[j]=x%10;
+        tmp=x/10;
       }
-      while (dgt_tmp>0) {
-        dgt_arr[dgt_m]=dgt_tmp%10;
-        dgt_tmp/=10;
-        ++dgt_m;
+      while (tmp>0) {
+        arr[m]=tmp%10;
+        tmp/=10;
+        ++m;
       }
     }
-    for (dgt_i=dgt_m-1; dgt_i>=0; --dgt_i)
-      printf("%d", dgt_arr[dgt_i]);
+    for (i=m-1; i>=0; --i)
+      printf("%d", arr[i]);
   }
   putchar('\n');
   return(0);
 }
 
 int dgt_max = 5000;
-void dgt_factorial(int a[], int n)
+void dgt_fact(int a[], int n)
 {
   /* factorial in array */
   if (!n) 
     return;
   int carry=0;
   for (int i=dgt_max-1; i>=0; --i) {
-    arr[i]=(a[i]*n)+carry;
+    a[i]=(a[i]*n)+carry;
     carry=a[i]/10;
     a[i]%=10;
   }
-  dgt_factorial(arr, n-1);
+  dgt_fact(a, n-1);
 }
 
-void dgt_display(int a[])
+void dgt_disp(int a[])
 {
   /* to print array */
-  int c=0;
-  int i=0;
+  int i=0, c=0;
   for (i=0; i<dgt_max; ++i) {
     if (!c && a[i])      
       c=1;
@@ -62,9 +63,10 @@ int dgt_main(int num)
   a[dgt_max-1]=1;
   //int num = 100;
   //printf("factorial of  %d is: ", num);
-  dgt_factorial(a, num);
-  dgt_display(a);
+  dgt_fact(a, num);
+  dgt_disp(a);
+  putchar('\n');
   free(a);
   return(0);
 }
-#endif 
+#endif /* DIGITS_H */

a475517b3a5dc45174022bb3a79861c98cdb096c
ryankeleti <elitebananart@gmail.com>
2016-11-27 08:06:25 -0500

    update Sun Nov 27 08:06:25 EST 2016

---
+++ olifemod/sm/slife3.c
@@ -0,0 +1,5 @@
+#define Z A[p][i*32+j
+#define Y for(i=0;i++<31;puts(""))for(j=0;j++<31;)
+A[2][1024],j,s,p;main(i){Y Z]=rand()&1;for(;sleep(1),puts("\e[2J");p=1-p)Y
+s=Z-33]+Z-32]+Z-31]+Z-1]+Z+1]+Z+31]+Z+32]+Z+33],A[1-p][i*32+j]=s==3|
+s==2&Z],printf(Z]?"[]":"  ");}
---
+++ olifemod/sm/slife4.c
@@ -0,0 +1,5 @@
+#define Z+A[p][i*32+j
+#define Y for(i=0;i++<31;puts(""))for(j=0;j++<31;)
+A[2][1024],j,p;main(i){Y A[p][i*32+j]=rand()&1;for(;sleep(1),puts("\e[2J");
+p=!p)Y printf((A[!p][i*32+j]=(Z-33]Z-32]Z-31]Z-1]Z+1]Z+31]Z+32]Z+33]|Z])==3)
+?"[]":"  ");}

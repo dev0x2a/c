55ef6b77b7729ab03fffeca53fdf5d9abadbb7bb
ryankeleti <elitebananart@gmail.com>
2016-11-28 21:08:04 -0500

    update Mon Nov 28 21:08:04 EST 2016

--- primes/rbignum.h
+++ primes/rbignum.h
@@ -24,6 +24,8 @@ typedef unsigned int uint;
 #define NMIN(_a,_b) ((_a<_b)?(_a):(_b))
 #endif
 
+#define BASE 16
+
 typedef struct {
   int sign;
   int size;
@@ -62,5 +64,70 @@ bignum decryptrsa(bignum c, bignum d, bignum n);
  * apply decrypt alg w/ priv key (d,n)
  * check orig msg was recovered */
 void testrsa(int len);
+
+
+
+/* 
+ * notes
+ *
+ * RAM
+ * m[0],m[1],m[2],... 
+ * Instructions
+ * I_0,I_1,...
+ *
+ *
+ * arithmetic
+ * a <- b * g (where * denotes an interger operation)
+ *    ie +,-,x,floor(./.)
+ * b and g are of the form c, m[a], m[m[a]]
+ * (where c is an int constant, a is an unsigned int constant)
+ * b * g ->eval->store in a
+ *
+ * branching
+ *
+ * IF b <> g GOTO i
+ * (where i is the index of an instruction, and <> a comparison operator
+ *   =, !=, <, >, <=, >= and b,g are as above)
+ * passes flow of control conditionally to instruction I_i
+ *
+ * halt
+ * halts execution 
+ *
+ * base-B
+ */
+int table[489720];
+
+int lookup(unsigned char *key)
+{
+  int index = hash(key);
+  return table[index];
+}
+
+struct entry {
+  unsigned char *key;
+  int value;
+  struct entry *next;
+} *table[1<<20];
+
+int lookup(unsigned char *key){
+  int index = hash(key) % (1<<20);
+  
+  for (struct entry *e=table[index]; e!=NULL; e=e->next) {
+    if (!strcmp(key, e->key))
+      return e->value;
+  }
+  // not found
+}
+
+int hashstring(const char *s)
+{
+  /* take the result % hash table size */
+  int key = 0;
+  while (*s) {
+    key = key*37 + *s++;
+  }
+  return key;
+}
+
 
 #endif

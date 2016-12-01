7aee1e07629b811d4f9c7374796dac0bed1d13fe
ryankeleti <elitebananart@gmail.com>
2016-12-01 07:11:39 -0500

    update Thu Dec  1 07:11:39 EST 2016

---
+++ primes/sackspiral.c
@@ -0,0 +1,9 @@
+/* r = sqrt n } every non-negative real number n
+ * theta = sqrt n }
+ * theta in rotations, not radians
+ * one rotation=360 deg
+ *
+ * x = r*cos(2*pi*theta)
+ * y = r*sin(2*pi*theta)
+ */
+
